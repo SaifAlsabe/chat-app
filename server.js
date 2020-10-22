1f556fa5a44c2d131423f01b6609fdf79468fb70
saifalsabe <saif.alsabe@gmail.com>
2020-10-22 18:39:18 -0400

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const server = app.listen(3000, () => { console.log('Listening at port 3000') });
+PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => { console.log('Listening at port 3000') });
 const io = require('socket.io')(server);
 
 rooms = {};
